89070c952c36e597810e0a633d493bfec3cae46b
曹玉昊 <1572294593@qq.com>
2023-09-12 22:51:32 +0800

    1

--- 学生管理系统/springdemo/src/main/java/com/example/springdemo/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springdemo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AdminService {
-
-    boolean validateUser(String username, String password);
-}
