6fa45b294078d800bf02b4693a33cd4c49559d1c
曹玉昊 <1572294593@qq.com>
2023-09-12 22:36:57 +0800

    前端代码

---
+++ 学生管理系统/vuedemo/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login' // 将根路径重定向到/login
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
